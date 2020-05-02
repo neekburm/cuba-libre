453f0d302b6e153cdd9935fd00f7a2032ecf7eb2
Scott Johnson <sjohnson@pivvot.com>
2020-05-02 12:55:44 -0500

    Initial commit

---
+++ lib/cubalibre.ex
@@ -0,0 +1,18 @@
+defmodule Cubalibre do
+  @moduledoc """
+  Documentation for `Cubalibre`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cubalibre.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/cubalibre_test.exs
@@ -0,0 +1,8 @@
+defmodule CubalibreTest do
+  use ExUnit.Case
+  doctest Cubalibre
+
+  test "greets the world" do
+    assert Cubalibre.hello() == :world
+  end
+end
